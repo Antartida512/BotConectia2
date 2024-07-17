7a12704ea6342058ad52e5184a9ed0214e6cfe31
Antartida512 <raza79lol@gmail.com>
2024-07-17 14:38:49 -0300

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-const { createBot, createProvider, createFlow, addKeyword, EVENTS, addAnswer } = require('@bot-whatsapp/bot');
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot');
+const { MongoClient } = require('mongodb');
 const openaiApiKey = process.env.OPENAI_API_KEY;
 const mongoDbUri = process.env.MONGO_DB_URI;
 
@@ -24,9 +25,16 @@ const ameneties = fs.readFileSync(amenetiesPath, "utf8");
 const uaePath = path.join(__dirname, 'mensajes', 'uae.txt');
 const uae = fs.readFileSync(uaePath, "utf8");
 
-// MENU
+// Conexión a la base de datos MongoDB
+const client = new MongoClient(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const menuFlow = addKeyword("Menu").addAnswer(
+async function connectToDatabase() {
+    await client.connect();
+    return client.db('BaseTest');
+}
+
+// MENU
+const menuFlow = addKeyword("Menu, Menú").addAnswer(
     menu,
     { capture: true },
     async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
@@ -51,40 +59,59 @@ const menuFlow = addKeyword("Menu").addAnswer(
             case "7":
                 return gotoFlow(flowReservar);
             case "0":
-                return await flowDynamic(
-                    "Saliendo... Puedes volver a acceder a este menú escribiendo '*Menu*'"
-                );
+                await flowDynamic("Muchas gracias por su consulta!");
+                return;
         }
     }
 );
 
+// Modificación del flujo de bienvenida para capturar el correo electrónico
+const flowCaptureEmail = addKeyword('CAPTURE_EMAIL')
+    .addAnswer('Por favor, ¿nos podrías compartir tu correo electrónico?', { capture: true }, async (ctx, { flowDynamic, gotoFlow }) => {
+        const email = ctx.body.trim();
+        if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            const db = await connectToDatabase();
+            await db.collection('usuarios').insertOne({ email });
+            await flowDynamic('¡Gracias! Ahora puedes escribir *Menu* para continuar.');
+        } else {
+            await flowDynamic('Correo electrónico no válido. Por favor, ingresa un correo electrónico válido.');
+            return gotoFlow(flowCaptureEmail); // Volver a pedir el correo electrónico si es inválido
+        }
+    });
+
+const flowWelcome = addKeyword(EVENTS.WELCOME)
+    .addAnswer('🙌 Hola mi nombre es Juana, soy la asistente virtual de Conectia, estoy aquí para brindarte la mejor atención.')
+    .addAnswer('Por favor, ¿nos podrías indicar tu nombre?', { capture: true }, async (ctx, { gotoFlow }) => {
+        return gotoFlow(flowCaptureEmail);
+    });
+
 const flowInfo = addKeyword(EVENTS.ACTION)
     .addAnswer('Seleccionaste "Información General". A continuación te brindo los detalles:')
     .addAnswer(info)
-    .addAnswer("Escriba *MENU* para volver a ver las opciones");
+    .addAnswer("Escribe *MENU* para volver a ver las opciones.");
 
 const flowPreciosyfinanciamiento = addKeyword(EVENTS.ACTION)
-    .addAnswer('Seleccionaste  *Precios y Financiamientos*. A continuación te brindo más detalles:')
+    .addAnswer('Seleccionaste *Precios y Financiamientos*. A continuación te brindo más detalles:')
     .addAnswer(financiamiento)
     .addAnswer("Te envío los valores de los departamentos disponibles", {
         media: "https://i.ibb.co/Czr5s0h/Whats-App-Image-2024-06-25-at-11-47-10.jpg"
     })
-    .addAnswer("Escriba *MENU* para volver a ver las opciones");
+    .addAnswer("Escribe *MENU* para volver a ver las opciones.");
 
 const flowAmeneties = addKeyword(EVENTS.ACTION)
-    .addAnswer('Seleccionaste *Amenities*. A continuación te brindo mas detalles')
+    .addAnswer('Seleccionaste *Amenities*. A continuación te brindo más detalles:')
     .addAnswer(ameneties)
-    .addAnswer("Escriba *MENU* para volver a ver las opciones");
+    .addAnswer("Escribe *MENU* para volver a ver las opciones.");
 
 const flowBrochure = addKeyword(EVENTS.ACTION)
-    .addAnswer('Seleccionaste *Brochure* A continuación te brindo mas detalles', {
+    .addAnswer('Seleccionaste *Brochure*. A continuación te brindo más detalles:', {
         media: "https://conectiaba.com.ar/wp-content/uploads/2024/04/Brochure-conectia-WEB_compressed.pdf"
     })
-    .addAnswer("Escriba *MENU* para volver a ver las opciones");
+    .addAnswer("Escribe *MENU* para volver a ver las opciones.");
 
 const flowTipologias = addKeyword(EVENTS.ACTION)
-    .addAnswer('Seleccionaste *Tipologías* A continuacion te brindo mas detalles')
-    .addAnswer('Te envío las tipologías de los departamentos disponibles')
+    .addAnswer('Seleccionaste *Tipologías*. A continuación te brindo más detalles:')
+    .addAnswer('Te envío las tipologías de los departamentos disponibles:')
     .addAnswer("Monoambiente 53m2 Totales", {
         media: "https://i.ibb.co/7G9BXJ0/Tipologia-1-ambiente-53m2.jpg"
     })
@@ -94,25 +121,16 @@ const flowTipologias = addKeyword(EVENTS.ACTION)
     .addAnswer('2 Ambientes 62m2 Totales', {
         media: "https://i.ibb.co/7SLmBHD/Tipolog-a-2-ambientes-62m2.jpg"
     })
-    .addAnswer("Escriba *MENU* para volver a ver las opciones");
+    .addAnswer("Escribe *MENU* para volver a ver las opciones.");
 
 const flowUae = addKeyword(EVENTS.ACTION)
-    .addAnswer("Seleccionaste Accesibilidad y Entorno*. A continuación te brindo mas detalles")
+    .addAnswer('Seleccionaste *Accesibilidad y Entorno*. A continuación te brindo más detalles:')
     .addAnswer(uae)
-    .addAnswer("Escriba *MENU* para volver a ver las opciones");
+    .addAnswer("Escribe *MENU* para volver a ver las opciones.");
 
 const flowReservar = addKeyword(EVENTS.ACTION)
-    .addAnswer('Muchas gracias en breve un asesor se comunicara con usted')
-    .addAnswer("Escriba *MENU* para volver a ver las opciones");
-
-const flowWelcome = addKeyword(EVENTS.WELCOME)
-    .addAnswer('🙌*Hola mi nombre es Juana, soy la asistente virtual de Conectia estoy aca para brindarte la mejor atención*')
-    .addAnswer('Por favor ¿nos podrías compartir tu correo electrónico?', {
-        delay: 3000,
-    })
-    .addAnswer('Escribí *Menu* para avanzar', {
-        delay: 3000,
-    });
+    .addAnswer('Muchas gracias, en breve un asesor se comunicará contigo.')
+    .addAnswer("Escribe *MENU* para volver a ver las opciones.");
 
 const main = async () => {
     const adapterDB = new MongoAdapter({
@@ -129,7 +147,8 @@ const main = async () => {
         flowPreciosyfinanciamiento,
         flowUae,
         flowAmeneties,
-        flowTipologias
+        flowTipologias,
+        flowCaptureEmail
     ]);
 
     const adapterProvider = createProvider(BaileysProvider);
@@ -144,3 +163,4 @@ const main = async () => {
 }
 
 main();
+
